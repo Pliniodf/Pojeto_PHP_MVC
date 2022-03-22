952035026d1a4709406f76f9e581af81f6def9b3
Plinio <plininhodf@gmail.com>
2022-03-22 11:20:20 -0300

    Substituir link usando bindValue

--- app/sts/Models/StsHome.php
+++ app/sts/Models/StsHome.php
@@ -9,12 +9,23 @@ if (!defined('URL')){
 
 class StsHome
 {
+    private $Resultado;
+
     public function index()
     {
+
         //echo "Listar dados <br>";
         //$conexao = new \Sts\Models\helper\StsConn();
         //$conexao->getConn();
         $listar = new \Sts\Models\helper\StsRead();
-        $listar->exeRead('sts_carousels', 'WHERE adms_situacoes_id =:adms_situacoes_id LIMIT :limit', 'adms_situacoes_id=1&limit=4');
+
+        //listar contendo todas as informaçoes
+        //$listar->exeRead('sts_carousels', 'WHERE adms_situacoes_id =:adms_situacoes_id LIMIT :limit', 'adms_situacoes_id=1&limit=4');
+
+        //listar filtrando as palavras nome e link
+        $listar->fullRead("SELECT nome, link FROM sts_carousels WHERE adms_situacoes_id =:adms_situacoes_id LIMIT :limit", 'adms_situacoes_id=1&limit=4');
+        $this->Resultado['sts_carousels'] = $listar->getResultado();
+        //var_dump($this->Resultado['sts_carousels']);
+        return $this->Resultado['sts_carousels'];
     }
 }
